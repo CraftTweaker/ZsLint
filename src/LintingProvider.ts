813e86514338e668f4499dc1d0bcdcf984824f6a
BloodWorkXGaming <BloodWorkXGaming@users.noreply.github.com>
2018-05-22 02:50:08 +0200

    Added files

---
+++ src/LintingProvider.ts
@@ -0,0 +1,117 @@
+import * as vscode from 'vscode';
+import * as WebSocket from "ws";
+import { Disposable } from 'vscode';
+
+
+function replaceAll(value: string, search: string, replace: string) {
+    return value.replace(new RegExp('[' + search + ']', 'g'), replace);
+}
+
+export default class LintingProvider {
+    private _disposable!: vscode.Disposable;
+    private socket!: WebSocket;
+    private diagnosticCollection!: vscode.DiagnosticCollection;
+
+    public activate(subscriptions: vscode.Disposable[]) {
+        subscriptions.push(this);
+
+        this.diagnosticCollection = vscode.languages.createDiagnosticCollection();
+
+        let disp: Disposable[] = [];
+
+        vscode.workspace.onDidSaveTextDocument(this.doZSLint, this, disp);
+
+        this._disposable = Disposable.from(...disp);
+        this.socket = new WebSocket("ws://127.0.0.1:24532", "zslint");
+
+        console.log("ZsLintingPlugin now activated");
+    }
+
+
+    public dispose(): void {
+        this._disposable.dispose();
+        this.socket.close();
+    }
+
+
+    private doZSLint(textDocument: vscode.TextDocument) {
+        if (textDocument.languageId !== 'zenscript') {
+            return;
+        }
+
+        if (this.socket.CLOSED === this.socket.readyState) {
+            console.log("Socket is closed, trying to open new one.");
+            this.socket = new WebSocket("ws://127.0.0.1:24532", "zslint");
+
+            this.socket.onopen = (event: { target: WebSocket }) => { this.startSocketCom(event.target); };
+        } else {
+            this.startSocketCom(this.socket);
+        }
+
+
+    }
+
+    private startSocketCom(s: WebSocket) {
+        let m = {
+            messageType: "LintRequest"
+        };
+
+        let rootDir: string = vscode.workspace.workspaceFolders !== undefined ? vscode.workspace.workspaceFolders[0].uri.path : "";
+        rootDir = rootDir.substr(1) + "/";
+        console.log("root: " + rootDir);
+
+
+        this.socket.send(JSON.stringify(m));
+
+        this.socket.onmessage = (event: { data: WebSocket.Data; type: string; target: WebSocket }) => {
+            console.log("Received message: " + event.data + " of type: " + event.type);
+
+            let diagnostics: { diagnostic: vscode.Diagnostic, fileName: string }[] = [];
+
+
+            let obj = JSON.parse(event.data as string);
+
+            if (obj.messageType === "LintResponse") {
+                let lintRes = obj as ILintResponse;
+
+                lintRes.errors.forEach(element => {
+                    let range = new vscode.Range(element.line - 1, element.offset - 1, element.line - 1, element.offset + 3);
+                    let severity = (element.level === "ERROR") ? vscode.DiagnosticSeverity.Error : vscode.DiagnosticSeverity.Warning;
+                    let diagnostic = new vscode.Diagnostic(range, element.explanation, severity);
+                    let fileName = element.fileName;
+                    diagnostics.push({ diagnostic, fileName });
+                });
+
+                vscode.workspace.textDocuments.forEach(element => {
+                    let ds: vscode.Diagnostic[] = [];
+                    diagnostics.forEach(diag => {
+                        const fileName1 = replaceAll(element.fileName, "\\\\", "/");
+                        const fileName2 = rootDir + replaceAll(replaceAll(diag.fileName, "\\\\", "/"), "\/\/", "/");
+
+                        console.log("filename1: " + fileName1);
+                        console.log("filename2: " + fileName2);
+
+                        if (fileName1 === fileName2) {
+                            ds.push(diag.diagnostic);
+                        }
+                    });
+
+                    this.diagnosticCollection.set(element.uri, ds);
+                });
+
+            }
+        };
+    }
+}
+
+interface ILintResponse {
+    errors: {
+        fileName: string;
+        line: number;
+        offset: number;
+        explanation: string;
+        level: "ERROR" | "INFO" | "WARN";
+    }[];
+    loadSuccessful: boolean;
+    messageType: "LintResponse";
+}
---
+++ src/extension.ts
@@ -0,0 +1,101 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the necessary extensibility types to use in your code below
+import { window, commands, Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument } from 'vscode';
+import LintingProvider from './LintingProvider';
+
+// This method is called when your extension is activated. Activation is
+// controlled by the activation events defined in package.json.
+export function activate(context: ExtensionContext) {
+
+    // Use the console to output diagnostic information (console.log) and errors (console.error).
+    // This line of code will only be executed once when your extension is activated.
+    console.log('Congratulations, your extension "WordCount" is now active!');
+
+    // create a new word counter
+    let wordCounter = new WordCounter();
+    let controller = new WordCounterController(wordCounter);
+    let linter = new LintingProvider();
+
+    linter.activate(context.subscriptions);
+
+    // Add to a list of disposables which are disposed when this extension is deactivated.
+    context.subscriptions.push(wordCounter);
+    context.subscriptions.push(controller);
+}
+
+class WordCounter {
+
+    private _statusBarItem: StatusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
+
+    public updateWordCount() {
+
+        // Get the current text editor
+        let editor = window.activeTextEditor;
+        if (!editor) {
+            this._statusBarItem.hide();
+            return;
+        }
+
+        let doc = editor.document;
+
+        // Only update status if an Markdown file
+        if (doc.languageId === "markdown") {
+            let wordCount = this._getWordCount(doc);
+
+            // Update the status bar
+            this._statusBarItem.text = wordCount !== 1 ? `$(pencil)${wordCount} Words` : '$(pencil)1 Word';
+            this._statusBarItem.show();
+        } else {
+            this._statusBarItem.hide();
+        }
+    }
+
+    public _getWordCount(doc: TextDocument): number {
+
+        let docContent = doc.getText();
+
+        // Parse out unwanted whitespace so the split is accurate
+        docContent = docContent.replace(/(< ([^>]+)<)/g, '').replace(/\s+/g, ' ');
+        docContent = docContent.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+        let wordCount = 0;
+        if (docContent !== "") {
+            wordCount = docContent.split(" ").length;
+        }
+
+        return wordCount;
+    }
+
+    dispose() {
+        this._statusBarItem.dispose();
+    }
+}
+
+
+class WordCounterController {
+
+    private _wordCounter: WordCounter;
+    private _disposable: Disposable;
+
+    constructor(wordCounter: WordCounter) {
+        this._wordCounter = wordCounter;
+
+        // subscribe to selection change and editor activation events
+        let subscriptions: Disposable[] = [];
+        window.onDidChangeTextEditorSelection(this._onEvent, this, subscriptions);
+        window.onDidChangeActiveTextEditor(this._onEvent, this, subscriptions);
+
+        // update the counter for the current file
+        this._wordCounter.updateWordCount();
+
+        // create a combined disposable from both event subscriptions
+        this._disposable = Disposable.from(...subscriptions);
+    }
+
+    dispose() {
+        this._disposable.dispose();
+    }
+
+    private _onEvent() {
+        this._wordCounter.updateWordCount();
+    }
+}
